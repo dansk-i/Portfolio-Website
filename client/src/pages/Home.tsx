a61a9a33613bd30dc5fa5350257c75aa4bf5589d
Daniel Sobalski <133881590+dansk-i@users.noreply.github.com>
2025-07-23 14:35:00 -0400

    Populate header card; Setup image folder and import profile picture

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import { useEffect } from 'react';
 import './Home.css'; // optional future styling
+import profilePic from '../assets/testimage.png'; 
 
 export default function Home() {
   
@@ -13,21 +14,26 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center">
       <main className="text-white m-auto grid grid-cols-4 grid-rows-8 gap-5 max-w-6xl overflow-hidden w-full h-full p-6 h-screen max-h-[800px]">
             
-            <div className="bg-neutral-800 border border-zinc-600 col-span-3 row-span-4 align-start flex-none justify-start p-6 rounded-lg hover:border-red-500 hover:border-2 transition duration-200"> 
-              <div className="">
-                  
+          {/* Main Header */}
+          <div className="flex w-full h-full bg-neutral-800 border border-zinc-600 col-span-3 row-span-4 align-start justify-start p-6 rounded-lg hover:border-red-500 hover:border-1 transition duration-200"> 
+              <div className="flex flex-col justify-between">
                   <div className=''>
-                    <h2 className="text-sm font-light m-0 text-zinc-500">welcome</h2>
+                      <h2 className="text-sm font-light m-0 text-zinc-500">welcome</h2>
                   </div>
-                  <div className=''>
-                    <h1>I'm <span className="font-bold">Daniel Sobalski</span>, a software developer with strong focus on graphics programming</h1>
+                  <div className='h-full'>
+                      <h1>I'm <span className="font-bold">Daniel Sobalski</span>, a software developer with strong focus on graphics programming</h1>
                   </div>
-               
+
               </div>
+                  
+                  <img src={profilePic} alt='Profile' className='w-auto max-h-[300px] select-none border-2'/>
+                        
             </div>
        
+
+       
         {/* About Me */}
-        <section className="bg-neutral-800 border border-zinc-600 align-start flex-none h-full justify-start relative col-span-1 row-span-7 p-6 rounded-lg hover:border-red-500 hover:border-2 transition duration-200">
+        <section className="bg-neutral-800 border border-zinc-600 align-start flex-none h-full justify-start relative col-span-1 row-span-7 p-6 rounded-lg hover:border-red-500 hover:border-1 transition duration-200">
           <Link to="/projects" className="nav-card">
             <h2>Projects</h2>
             <p>See my portfolio projects</p>
@@ -41,32 +47,32 @@ export default function Home() {
        
 
          {/* Email And Socials */}
-        <section className="bg-neutral-800 border border-zinc-600 align-start flex-none h-full justify-start relative col-span-1 row-span-4 p-6 rounded-lg hover:border-red-500 hover:border-2 transition duration-200">
+        <section className="bg-neutral-800 border border-zinc-600 align-start flex-none h-full justify-start relative col-span-1 row-span-4 p-6 rounded-lg hover:border-red-500 hover:border-1 transition duration-200">
            <h2>Contact Me!</h2>
         </section>
 
          {/* Clock */}
-        <section className="bg-neutral-800 border border-zinc-600 align-start flex-none h-full justify-start relative col-span-1 row-span-1 p-6 rounded-lg hover:border-red-500 hover:border-2 transition duration-200">
+        <section className="bg-neutral-800 border border-zinc-600 align-start flex-none h-full justify-start relative col-span-1 row-span-1 p-6 rounded-lg hover:border-red-500 hover:border-1 transition duration-200">
            <h2>Time Temp</h2>
         </section>
 
          {/* Projects */}
-        <section className="bg-neutral-800 border border-zinc-600 align-start flex-none h-full justify-start relative col-span-1 row-span-3 p-6 rounded-lg hover:border-red-500 hover:border-2 transition duration-200">
+        <section className="bg-neutral-800 border border-zinc-600 align-start flex-none h-full justify-start relative col-span-1 row-span-3 p-6 rounded-lg hover:border-red-500 hover:border-1 transition duration-200">
            <h2>Projects</h2>
         </section>
 
          {/* Now Status */}
-        <section className="bg-neutral-800 border border-zinc-600 align-start flex-none h-full justify-start relative col-span-1 row-span-3 p-6 rounded-lg hover:border-red-500 hover:border-2 transition duration-200">
+        <section className="bg-neutral-800 border border-zinc-600 align-start flex-none h-full justify-start relative col-span-1 row-span-3 p-6 rounded-lg hover:border-red-500 hover:border-1 transition duration-200">
            <h2>Now?</h2>
         </section>
 
           {/* Colors */}
-        <section className="bg-neutral-800 border border-zinc-600 align-start flex-none h-full justify-start relative col-span-1 row-span-1 p-6 rounded-lg hover:border-red-500 hover:border-2 transition duration-200">
+        <section className="bg-neutral-800 border border-zinc-600 align-start flex-none h-full justify-start relative col-span-1 row-span-1 p-6 rounded-lg hover:border-red-500 hover:border-1 transition duration-200">
            <h2>Colors</h2>
         </section>
 
           {/* Trademark */}
-        <section className="bg-neutral-800 border border-zinc-600 align-start flex-none h-full justify-start relative col-span-1 row-span-1 p-6 rounded-lg hover:border-red-500 hover:border-2 transition duration-200">
+        <section className="bg-neutral-800 border border-zinc-600 align-start flex-none h-full justify-start relative col-span-1 row-span-1 p-6 rounded-lg hover:border-red-500 hover:border-1 transition duration-200">
            <h2>Trademark</h2>
         </section>
 
