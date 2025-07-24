3c774161a35f971e1d7f5b37d509cd3faa95c5bb
Daniel Sobalski <133881590+dansk-i@users.noreply.github.com>
2025-07-23 20:39:50 -0400

    Add and configure external url logo buttons for home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,8 @@
 import { Link } from 'react-router-dom';
 import { useEffect } from 'react';
-import './Home.css'; // optional future styling
+import './Home.css';
 import profilePic from '../assets/testimage.png'; 
+import {ReactComponent as GithubLogo} from '../assets/github.svg';
 
 export default function Home() {
   
@@ -15,13 +16,30 @@ export default function Home() {
       <main className="text-white m-auto grid grid-cols-4 grid-rows-8 gap-5 max-w-6xl overflow-hidden w-full h-full p-6 h-screen max-h-[800px]">
             
           {/* Main Header */}
-          <div className="flex w-full h-full bg-neutral-800 border border-zinc-600 col-span-3 row-span-4 align-start justify-start p-6 rounded-lg hover:border-red-500 hover:border-1 transition duration-200"> 
+          <div className="flex w-full h-full bg-neutral-800 border border-zinc-600 col-span-3 row-span-4 align-start justify-start p-6 pb-10 rounded-lg hover:border-red-500 hover:border-1 transition duration-200"> 
               <div className="flex flex-col justify-between">
                   <div className=''>
                       <h2 className="text-sm font-light m-0 text-zinc-500">welcome</h2>
                   </div>
                   <div className='h-full'>
                       <h1>I'm <span className="font-bold">Daniel Sobalski</span>, a software developer with strong focus on graphics programming</h1>
+                  </div>
+
+                    {/* Logo Links */}
+                  <div className='flex flex-row gap-4 justify-start place-items-center'>
+
+                    <div className='bg-neutral-950 w-fit px-5 py-3 border border-red-500 border-1 border-r-[3px] border-b-[3px] transition duration-200 rounded-lg text-white hover:text-red-500'>
+                        <a href="https://github.com/" target="_blank" rel="noopener noreferrer">
+                             <GithubLogo className="w-5 h-4"/>
+                        </a>
+                    </div>
+
+                    <div className='bg-neutral-950 w-fit px-5 py-3 border border-red-500 border-1 border-r-[3px] border-b-[3px] transition duration-200 rounded-lg text-white hover:text-red-500'>
+                        <a href="https://github.com/" target="_blank" rel="noopener noreferrer">
+                             <GithubLogo className="w-5 h-4"/>
+                        </a>
+                    </div>
+
                   </div>
 
               </div>
