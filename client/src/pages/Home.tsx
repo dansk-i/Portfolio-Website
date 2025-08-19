6c0b493d9b46c890b51c7728ebdbc19b51305190
Daniel Sobalski <133881590+dansk-i@users.noreply.github.com>
2025-08-18 23:19:14 -0400

    Add dynamic border color functionality to Home component

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -11,10 +11,19 @@ import dotsAni from '../assets/dots.gif';
 import codingAni from '../assets/coding.gif';
 
 
+
 export default function Home() {
 
   const [showFact, setShowFact] = useState(false);
   const [factIndex, setFactIndex] = useState(0);
+
+  const [bordColor, setBordColor] = useState("red");
+  const colorClasses: Record<string, { border: string; text: string; hover: string; groupHover: string; active: string; after: string }> = {
+  red:   { border: "border-rose-500", text: "hover:text-rose-500", hover: "hover:border-rose-500", groupHover: "group-hover:text-rose-500", active: "active:text-rose-500", after: "after:border-rose-500"},
+  blue:  { border: "border-cyan-500", text: "hover:text-cyan-500", hover: "hover:border-cyan-500", groupHover: "group-hover:text-cyan-500", active: "active:text-cyan-500", after: "after:border-cyan-500"},
+  green: { border: "border-emerald-500", text: "hover:text-emerald-500", hover: "hover:border-emerald-500", groupHover: "group-hover:text-emerald-500", active: "active:text-emerald-500", after: "after:border-emerald-500"},
+  violet: { border: "border-violet-500", text: "hover:text-violet-500", hover: "hover:border-violet-500", groupHover: "group-hover:text-violet-500", active: "active:text-violet-500", after: "after:border-violet-500"},
+};
 
   const facts = [
     "I love shaders",
@@ -45,7 +54,7 @@ export default function Home() {
       <main className="text-white m-auto grid grid-cols-4 grid-rows-8 gap-4 max-w-6xl overflow-hidden w-full h-full p-6 h-screen max-h-[800px] font-mono">
 
         {/* Main Header */}
-        <div className="flex w-full h-full bg-neutral-800 border border-zinc-600 col-span-3 row-span-4 align-start justify-start p-6 pb-10 rounded-lg overflow-hidden hover:border-red-500 hover:border-1 transition duration-200">
+        <div className={`flex w-full h-full bg-neutral-800 border border-zinc-600 col-span-3 row-span-4 align-start justify-start p-6 pb-10 rounded-lg overflow-hidden ${colorClasses[bordColor].hover} hover:border-1 transition duration-200`}>
           <div className="flex flex-col justify-between">
             <div className=''>
               <h2 className="text-sm font-light m-0 text-zinc-500">welcome</h2>
@@ -62,7 +71,7 @@ export default function Home() {
 
               <div className='w-20'>
                 <button onClick={() => window.open("https://github.com/dansk-i", "_blank")}
-                  className="bg-neutral-950 w-fit px-5 py-3 border border-red-500 border-1 border-r-[3px] border-b-[3px] transition duration-200 rounded-bl-xl rounded-tl-lg rounded-br-lg rounded-tr-xl text-white hover:text-red-500 active:border-r-[1px] active:border-b-[1px] active:text-red-500" >
+                  className={`bg-neutral-950 w-fit px-5 py-3 border ${colorClasses[bordColor].border} border-1 border-r-[3px] border-b-[3px] transition duration-200 rounded-bl-xl rounded-tl-lg rounded-br-lg rounded-tr-xl text-white ${colorClasses[bordColor].text} active:border-r-[1px] active:border-b-[1px] ${colorClasses[bordColor].active}`}>
                   <GithubLogo className="w-6 h-5" />
                 </button>
               </div>
@@ -70,7 +79,7 @@ export default function Home() {
 
               <div className='w-20'>
                 <button onClick={() => window.open("https://www.linkedin.com/in/danielsobalski/", "_blank")}
-                  className="bg-neutral-950 w-fit px-5 py-3 border border-red-500 border-1 border-r-[3px] border-b-[3px] transition duration-200 rounded-bl-xl rounded-tl-lg rounded-br-lg rounded-tr-xl text-white hover:text-red-500 active:border-r-[1px] active:border-b-[1px] active:text-red-500" >
+                  className={`bg-neutral-950 w-fit px-5 py-3 border ${colorClasses[bordColor].border} border-1 border-r-[3px] border-b-[3px] transition duration-200 rounded-bl-xl rounded-tl-lg rounded-br-lg rounded-tr-xl text-white ${colorClasses[bordColor].text} active:border-r-[1px] active:border-b-[1px] ${colorClasses[bordColor].active}`}>
                   <LinkedinLogo className="w-6 h-5" />
                 </button>
               </div>
@@ -81,15 +90,15 @@ export default function Home() {
                 {showFact && (
 
                   <div className='absolute bottom-16 -translate-x-[69px] w-[200px] max-w-[200px]'>
-                    <div className="bg-neutral-950 px-5 py-3 border border-red-500 border-1 border-r-[3px] border-b-[3px] rounded-bl-xl rounded-tl-lg rounded-br-lg rounded-tr-xl text-white text-center
-                                    after:rotate-45 after:w-5 after:h-5 after:absolute after:bottom-[-8px] after:left-1/2 after:-translate-x-2 after:bg-neutral-950 after:border-r-[1px] after:border-b-[1px] after:border-red-500">
+                    <div className={`bg-neutral-950 px-5 py-3 border border-red-500 border-1 border-r-[3px] border-b-[3px] rounded-bl-xl rounded-tl-lg rounded-br-lg rounded-tr-xl text-white text-center
+                                    after:rotate-45 after:w-5 after:h-5 after:absolute after:bottom-[-8px] after:left-1/2 after:-translate-x-2 after:bg-neutral-950 after:border-r-[1px] after:border-b-[1px] after:border-red-500`}>
                       {facts[factIndex]}
                     </div>
                   </div>
                 )}
 
 
-                <button onMouseDown={handleMouseDown} onMouseUp={handleMouseUp} className="bg-neutral-950 w-fit px-5 py-3 border border-red-500 border-1 border-r-[3px] border-b-[3px] transition duration-200 rounded-bl-xl rounded-tl-lg rounded-br-lg rounded-tr-xl text-white hover:text-red-500 active:border-r-[1px] active:border-b-[1px] active:text-red-500" >
+                <button onMouseDown={handleMouseDown} onMouseUp={handleMouseUp} className={`bg-neutral-950 w-fit px-5 py-3 border ${colorClasses[bordColor].border} border-1 border-r-[3px] border-b-[3px] transition duration-200 rounded-bl-xl rounded-tl-lg rounded-br-lg rounded-tr-xl text-white ${colorClasses[bordColor].text} active:border-r-[1px] active:border-b-[1px] ${colorClasses[bordColor].active}`}>
                   <QuestionLogo className="w-6 h-5" />
                 </button>
               </div>
@@ -105,7 +114,7 @@ export default function Home() {
 
 
         {/* About Me */}
-        <section className="bg-neutral-800 border border-zinc-600 align-start flex-none h-full justify-start relative col-span-1 row-span-7 p-6 rounded-lg hover:border-red-500 hover:border-1 transition duration-200">
+        <section className={`bg-neutral-800 border border-zinc-600 align-start flex-none h-full justify-start relative col-span-1 row-span-7 p-6 rounded-lg ${colorClasses[bordColor].hover} hover:border-1 transition duration-200`}>
           <h2 className='text-lg font-bold pb-2'>About Me</h2>
           <div className='flex flex-col gap-2 text-sm font-light'>
             <p> Hi, I'm Daniel, a graphics programmer from Canada.</p> <br />
@@ -128,7 +137,7 @@ export default function Home() {
 
 
         {/* Email And Socials */}
-        <section className="bg-neutral-800 border border-zinc-600 align-start flex-none h-full justify-start relative col-span-1 row-span-4 p-6 rounded-lg hover:border-red-500 hover:border-1 transition duration-200">
+        <section className={`bg-neutral-800 border border-zinc-600 align-start flex-none h-full justify-start relative col-span-1 row-span-4 p-6 rounded-lg ${colorClasses[bordColor].hover} hover:border-1 transition duration-200`}>
           <h2 className='text-lg font-bold pb-5'>Let's start working together!</h2>
           <h2 className="italic text-zinc-500 pb-1">Contact Details</h2>
           <p className='italic text-sm pb-5'>daniel.sobalski2036@gmail.com <br />Canada</p>
@@ -142,7 +151,7 @@ export default function Home() {
         </section>
 
         {/* Clock */}
-        <section className="bg-neutral-800 overflow-hidden border border-zinc-600 align-center flex-none h-full justify-center relative col-span-1 row-span-1 p-6 rounded-lg hover:border-red-500 hover:border-1 transition duration-200 focus:outline-none focus:ring-2 focus:ring-red-500/40">
+        <section className={`bg-neutral-800 overflow-hidden border border-zinc-600 align-center flex-none h-full justify-center relative col-span-1 row-span-1 p-6 rounded-lg ${colorClasses[bordColor].hover} hover:border-1 transition duration-200 focus:outline-none focus:ring-2 focus:ring-red-500/40`}>
           <span className="absolute top-4 right-4 flex h-2 w-2">
             <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-500 opacity-50"></span>
             <span className="relative inline-flex rounded-full h-2 w-2 bg-red-600 opacity-75"></span>
@@ -152,12 +161,12 @@ export default function Home() {
         </section>
 
         {/* Projects */}
-        <Link to="/projects" draggable={false} className="group overflow-hidden relative bg-neutral-800 border border-zinc-600 align-start flex-none h-full justify-start col-span-1 row-span-2 p-2 rounded-lg hover:border-red-500 hover:border-1 transition duration-200">
-          <img src={codingAni} alt="" className="scale-[2] absolute inset-0 opacity-0 rounded-lg transition-opacity duration-150 group-hover:opacity-20 pointer-events-none" draggable={false} />
+        <Link to="/projects" draggable={false} className={`group overflow-hidden relative bg-neutral-800 border border-zinc-600 align-start flex-none h-full justify-start col-span-1 row-span-3 p-2 rounded-lg ${colorClasses[bordColor].hover} hover:border-1 transition duration-200`}>
+          <img src={codingAni} alt="" className="scale-[3] absolute inset-0 opacity-0 rounded-lg transition-opacity duration-150 group-hover:opacity-20 pointer-events-none" draggable={false} />
           <span className="pointer-events-none absolute right-5 top-5">
             <svg
-              className="h-5 w-5 text-zinc-400 transition-transform transition-colors duration-200
-                          ease-out origin-bottom-left group-hover:scale-[1.25] group-hover:text-red-500"
+              className={`h-5 w-5 text-zinc-400 transition-transform transition-colors duration-200
+                          ease-out origin-bottom-left group-hover:scale-[1.25] ${colorClasses[bordColor].groupHover}`}
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
@@ -171,7 +180,7 @@ export default function Home() {
             </svg>
           </span>
 
-          <section className='items-center justify-center w-full h-full flex opacity-75 hover:text-red-500 hover:opacity-65'>
+          <section className={`items-center justify-center w-full h-full flex opacity-75 ${colorClasses[bordColor].text} hover:opacity-65`}>
             <ProjectIcon className='absolute w-1/2 h-1/2' />
             <h2 className='text-md font-bold absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-5 '>Projects</h2>
             <img src={dotsAni} alt="" className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/3 w-1/2 h-1/2 inset-0 opacity-100 transition-opacity duration-150 group-hover:opacity-50 pointer-events-none" draggable={false} />
@@ -179,7 +188,7 @@ export default function Home() {
         </Link>
 
         {/* Now Status */}
-        <section className="bg-neutral-800 border border-zinc-600 align-start flex-none h-full justify-start relative col-span-1 row-span-3 p-6 rounded-lg hover:border-red-500 hover:border-1 transition duration-200">
+        <section className={`bg-neutral-800 border border-zinc-600 align-start flex-none h-full justify-start relative col-span-1 row-span-3 p-6 rounded-lg ${colorClasses[bordColor].hover} hover:border-1 transition duration-200`}>
 
           <div className='flex flex-col gap-2 p-2 pt-3'>
             <h3 className='text-md'>Now</h3>
@@ -190,19 +199,19 @@ export default function Home() {
         </section>
 
         {/* Colors */}
-        <section className="bg-neutral-800 border border-zinc-600 align-start flex-none h-full justify-start relative col-span-1 row-span-2 p-6 rounded-lg hover:border-red-500 hover:border-1 transition duration-200">
+        <section className={`bg-neutral-800 border border-zinc-600 align-start flex-none h-full justify-start relative col-span-1 row-span-1 p-6 rounded-lg ${colorClasses[bordColor].hover} hover:border-1 transition duration-200`}>
           <div className='w-full h-full flex flex-row justify-center items-center'>
               
-              <div className='p-1'><button className='w-10 h-10 rounded-full bg-white'></button></div>
-              <div className='p-1'><button className='w-10 h-10 rounded-full bg-red-500'></button></div>
-              <div className='p-1'><button className='w-10 h-10 rounded-full bg-blue-500'></button></div>
-              <div className='p-1'><button className='w-10 h-10 rounded-full bg-green-500'></button></div>
+              <div className='p-2'><button className='w-10 h-10 rounded-full bg-rose-500 hover:border-white hover:border-2' onClick={() => setBordColor("red")}></button></div>
+              <div className='p-2'><button className='w-10 h-10 rounded-full bg-violet-500 hover:border-white hover:border-2' onClick={() => setBordColor("violet")}></button></div>
+              <div className='p-2'><button className='w-10 h-10 rounded-full bg-cyan-500 hover:border-white hover:border-2' onClick={() => setBordColor("blue")}></button></div>
+              <div className='p-2'><button className='w-10 h-10 rounded-full bg-emerald-500 hover:border-white hover:border-2' onClick={() => setBordColor("green")}></button></div>
              
           </div>
         </section>
 
         {/* Trademark */}
-        <section className="bg-neutral-800 border border-zinc-600 align-start flex h-full justify-center relative col-span-1 row-span-1 p-8 rounded-lg hover:border-red-500 hover:border-1 transition duration-200">
+        <section className={`bg-neutral-800 border border-zinc-600 align-start flex h-full justify-center relative col-span-1 row-span-1 p-8 rounded-lg ${colorClasses[bordColor].hover} hover:border-1 transition duration-200`}>
           <p className='text-xs '>© 2025 · Daniel</p>
         </section>
 
